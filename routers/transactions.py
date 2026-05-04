5e4d53aa401d5fc5fa2529e005801eaa277cec62
Mariana Vila Nova Miranda <108958482+MaryUSA@users.noreply.github.com>
2026-05-03 21:43:54 -0300

    Create transactions.py

---
+++ routers/transactions.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.database import get_db
+from models.tables import Transaction
+from schemas.pydantic import TransactionCreate
+
+router = APIRouter()
+
+@router.post("/transactions/")
+async def create_tx(data: TransactionCreate, db: AsyncSession = Depends(get_db)):
+    new_tx = Transaction(**data.dict())
+    db.add(new_tx)
+    await db.commit()  # Operação assíncrona
+    await db.refresh(new_tx)
+    return new_tx
